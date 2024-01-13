14b5828bbd5a50eb5d75cf16b8e53f0118ae1290
Hame228 <78795653+Hame228@users.noreply.github.com>
2024-01-13 17:55:25 +0300

    Update UserDetails.jsq

--- UserDetails.js
+++ UserDetails.js
@@ -5,6 +5,8 @@ const UserDetailSchema = new mongoose.Schema(
     name: String,
     email: { type: String, unique: true },
     rickroll: String,
+    whistle: String,
+    lebiga: String,
     money: String,
   },
   {
