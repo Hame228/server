e4e827350190ff8ed1d70a8a6b96c33fb1d9d9f9
hame228 <dimabogatchuc@ukr.net>
2024-01-24 18:35:56 +0300

    Опис внесених змін

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+//const CronJob = require('cron');
+// Файл `background-task.js`
 
+const cron = require('node-cron');
+const http = require('http');
 
 app.use(express.json());
 app.use(cors());
@@ -13,6 +17,45 @@ const port = 3002;
 app.listen(port, () => {
   console.log(`Сервер запущено на порті ${port}`);
 });
+
+
+
+cron.schedule('*/5 * * * * *', () => {
+  console.log("aaaaa  ")
+  // Зробіть HTTP запит до сервера
+  const request = http.request({
+    url: 'https://slimeclicker.onrender.com',
+    method: 'GET',
+  });
+
+  request.on('response', (response) => {
+    // Обробте відповідь від сервера
+    if (response.statusCode === 200) {
+      console.log('Сервер доступний');
+    } else {
+      console.log('Сервер недоступний');
+    }
+  });
+
+  request.end();
+});
+
+
+
+/*const job = CronJob.from({
+	cronTime: '* * * * * *',
+	onTick: function () {
+		console.log('You will see this message every second');
+	},
+	start: true,
+	timeZone: 'America/Los_Angeles'
+});*/
+
+cron.schedule('* * * * *', () => {
+  console.log('running a task every minute');
+});
+
+
 
 const mongoUrl = "mongodb+srv://Hame:UmiJuJD2JDpkYiQ7@slimeclicker.eo5qn9j.mongodb.net/?retryWrites=true&w=majority"
 //const mongourl = "mongodb+srv://Hame:UmiJuJD2JDpkYiQ7@slimeclicker.eo5qn9j.mongodb.net/?retryWrites=true&w=majority"
@@ -32,7 +75,22 @@ app.get("/", (req, res) => {
   });
 
   app.post("/register", async (req, res) => {
-    const { name, email, rickroll, money } = req.body;
+    const { name, email, rickroll, money, whistle, lebiga, strange, activity,strangePrice,
+        CritDmg,
+        CritDmgPrice,
+        CritChance,
+        CritChancePrice,
+        Passive,
+        PassivePrice,
+        PassiveTime,
+        PassiveTimePrice,
+        Clicks,
+        Prestige,
+        PrestigePrice,
+        GoldenSlimeBuyed,
+        MinecraftSlimeBuyed,
+        EquipedSlime,
+          } = req.body;
     console.log(req.body);
   
     const oldUser = await User.findOne({ email: email });
@@ -47,7 +105,26 @@ app.get("/", (req, res) => {
         name: name,
         email: email,
         rickroll,
+        whistle,
+        lebiga,
         money:money,
+        strange:strange,
+        strangePrice:strangePrice,
+        CritDmg:CritDmg,
+        CritDmgPrice:CritDmgPrice,
+        CritChance:CritChance,
+        CritChancePrice:CritChancePrice,
+        Passive:Passive,
+        PassivePrice:PassivePrice,
+        PassiveTime:PassiveTime,
+        PassiveTimePrice:PassiveTimePrice,
+        activity: activity,
+        Clicks:Clicks,
+        Prestige:Prestige,
+        PrestigePrice:PrestigePrice,
+        GoldenSlimeBuyed:GoldenSlimeBuyed,
+        MinecraftSlimeBuyed:MinecraftSlimeBuyed,
+        EquipedSlime:EquipedSlime,
       });
       res.send({ status: "ok", data: "User Created" });
     } catch (error) {
@@ -120,9 +197,8 @@ app.get("/", (req, res) => {
     }*/
   });
 
-
   app.post("/videoss", async (req, res) => {
-    const {email, rickroll} = req.body;
+    const {email, rickroll, whistle, lebiga} = req.body;
     const oldUser = await User.findOne({ email: email });
     //console.log(oldUser)
     //const abo = await User.get
@@ -137,6 +213,8 @@ app.get("/", (req, res) => {
         //rickroll:rickroll
       }, {
         rickroll:rickroll,
+        whistle:whistle,
+        lebiga:lebiga
       });
       res.send({ status: "ok", data: "User Updated" });
     } catch (error) {
@@ -147,7 +225,7 @@ app.get("/", (req, res) => {
 
 
   app.post("/updateMoney", async (req, res) => {
-    const {email, money} = req.body;
+    const {email, money, Clicks} = req.body;
     const oldUser = await User.findOne({ email: email });
     //console.log(oldUser)
     //const abo = await User.get
@@ -162,6 +240,7 @@ app.get("/", (req, res) => {
         //rickroll:rickroll
       }, {
         money:money,
+        Clicks:Clicks,
       });
       res.send({ status: "ok", data: "User Updated" });
     } catch (error) {
@@ -169,6 +248,308 @@ app.get("/", (req, res) => {
     }
     
   });
+
+
+
+app.post("/updateSlime", async (req, res) => {
+    const {email, money, GoldenSlimeBuyed,MinecraftSlimeBuyed,EquipedSlime} = req.body;
+    const oldUser = await User.findOne({ email: email });
+    //console.log(oldUser)
+    //const abo = await User.get
+  
+    if (!oldUser) {
+      return res.send({ data: "User doesn't exists!!" });
+    }
+    try {
+      await User.updateOne({
+        email: email,
+        //email:email,
+        //rickroll:rickroll
+      }, {
+        money:money,
+        GoldenSlimeBuyed:GoldenSlimeBuyed,
+        MinecraftSlimeBuyed:MinecraftSlimeBuyed,
+        EquipedSlime:EquipedSlime,
+      });
+      res.send({ status: "ok", data: "User Updated" });
+    } catch (error) {
+      res.send({ status: "error", data: error });
+    }
+    
+  });
+
+
+
+
+app.post("/updateEquipedSlime", async (req, res) => {
+    const {email, money,EquipedSlime} = req.body;
+    const oldUser = await User.findOne({ email: email });
+    //console.log(oldUser)
+    //const abo = await User.get
+  
+    if (!oldUser) {
+      return res.send({ data: "User doesn't exists!!" });
+    }
+    try {
+      await User.updateOne({
+        email: email,
+        //email:email,
+        //rickroll:rickroll
+      }, {
+        money:money,
+        EquipedSlime:EquipedSlime,
+      });
+      res.send({ status: "ok", data: "User Updated" });
+    } catch (error) {
+      res.send({ status: "error", data: error });
+    }
+    
+  });
+
+
+
+app.post("/updateall", async (req, res) => {
+    const {email, money,strange,strangePrice,
+        CritDmg,
+        CritDmgPrice,
+        CritChance,
+        CritChancePrice,
+        Passive,
+        PassivePrice,
+        PassiveTime,
+        PassiveTimePrice,
+        Prestige,
+        PrestigePrice,} = req.body;
+    const oldUser = await User.findOne({ email: email });
+    //console.log(oldUser)
+    //const abo = await User.get
+  
+    if (!oldUser) {
+      return res.send({ data: "User doesn't exists!!" });
+    }
+    try {
+      await User.updateOne({
+        email: email,
+        //email:email,
+        //rickroll:rickroll
+      }, {
+        money:money,
+        strange:strange,
+        strangePrice:strangePrice,
+        CritDmg:CritDmg,
+        CritDmgPrice:CritDmgPrice,
+        CritChance:CritChance,
+        CritChancePrice:CritChancePrice,
+        Passive:Passive,
+        PassivePrice:PassivePrice,
+        PassiveTime:PassiveTime,
+        PassiveTimePrice:PassiveTimePrice,
+        Prestige:Prestige,
+        PrestigePrice:PrestigePrice,
+      });
+      res.send({ status: "ok", data: "User Updated" });
+    } catch (error) {
+      res.send({ status: "error", data: error });
+    }
+    
+  });
+
+
+
+
+ app.post("/updateStrange", async (req, res) => {
+    const {email,money, strange, strangePrice} = req.body;
+    const oldUser = await User.findOne({ email: email });
+    //console.log(oldUser)
+    //const abo = await User.get
+  
+    if (!oldUser) {
+      return res.send({ data: "User doesn't exists!!" });
+    }
+    try {
+      await User.updateOne({
+        email: email,
+        //email:email,
+        //rickroll:rickroll
+      }, {
+        money:money,
+        strange:strange,
+        strangePrice:strangePrice
+      });
+      res.send({ status: "ok", data: "User Updated" });
+    } catch (error) {
+      res.send({ status: "error", data: error });
+    }
+    
+  });
+
+
+ app.post("/updateCritDmg", async (req, res) => {
+    const {email,money, CritDmg, CritDmgPrice} = req.body;
+    const oldUser = await User.findOne({ email: email });
+    //console.log(oldUser)
+    //const abo = await User.get
+  
+    if (!oldUser) {
+      return res.send({ data: "User doesn't exists!!" });
+    }
+    try {
+      await User.updateOne({
+        email: email,
+        //email:email,
+        //rickroll:rickroll
+      }, {
+        money:money,
+        CritDmg:CritDmg,
+        CritDmgPrice:CritDmgPrice
+      });
+      res.send({ status: "ok", data: "User Updated" });
+    } catch (error) {
+      res.send({ status: "error", data: error });
+    }
+    
+  });
+
+
+
+
+ app.post("/updateCritChance", async (req, res) => {
+    const {email,money, CritChance,CritChancePrice} = req.body;
+    const oldUser = await User.findOne({ email: email });
+    //console.log(oldUser)
+    //const abo = await User.get
+  
+    if (!oldUser) {
+      return res.send({ data: "User doesn't exists!!" });
+    }
+    try {
+      await User.updateOne({
+        email: email,
+        //email:email,
+        //rickroll:rickroll
+      }, {
+        money:money,
+        CritChance:CritChance,
+        CritChancePrice:CritChancePrice
+      });
+      res.send({ status: "ok", data: "User Updated" });
+    } catch (error) {
+      res.send({ status: "error", data: error });
+    }
+    
+  });
+
+
+
+
+ app.post("/updatePassive", async (req, res) => {
+    const {email,money, Passive,PassivePrice} = req.body;
+    const oldUser = await User.findOne({ email: email });
+    //console.log(oldUser)
+    //const abo = await User.get
+  
+    if (!oldUser) {
+      return res.send({ data: "User doesn't exists!!" });
+    }
+    try {
+      await User.updateOne({
+        email: email,
+        //email:email,
+        //rickroll:rickroll
+      }, {
+        money:money,
+       Passive:Passive,
+       PassivePrice:PassivePrice
+      });
+      res.send({ status: "ok", data: "User Updated" });
+    } catch (error) {
+      res.send({ status: "error", data: error });
+    }
+    
+  });
+
+
+
+
+app.post("/updatePassiveTime", async (req, res) => {
+    const {email,money, PassiveTime,PassiveTimePrice} = req.body;
+    const oldUser = await User.findOne({ email: email });
+    //console.log(oldUser)
+    //const abo = await User.get
+  
+    if (!oldUser) {
+      return res.send({ data: "User doesn't exists!!" });
+    }
+    try {
+      await User.updateOne({
+        email: email,
+        //email:email,
+        //rickroll:rickroll
+      }, {
+        money:money,
+       PassiveTime:PassiveTime,
+       PassiveTimePrice:PassiveTimePrice
+      });
+      res.send({ status: "ok", data: "User Updated" });
+    } catch (error) {
+      res.send({ status: "error", data: error });
+    }
+    
+  });
+
+
+
+app.post("/updateActivity", async (req, res) => {
+    const {email,activity} = req.body;
+    const oldUser = await User.findOne({ email: email });
+    //console.log(oldUser)
+    //const abo = await User.get
+  
+    if (!oldUser) {
+      return res.send({ data: "User doesn't exists!!" });
+    }
+    try {
+      await User.updateOne({
+        email: email,
+        //email:email,
+        //rickroll:rickroll
+      }, {
+        activity:activity,
+      });
+      res.send({ status: "ok", data: "User Updated" });
+    } catch (error) {
+      res.send({ status: "error", data: error });
+    }
+    
+  });
+
+
+  app.post("/updateName", async (req, res) => {
+    const {email, name} = req.body;
+    const oldUser = await User.findOne({ email: email });
+    //console.log(oldUser)
+    //const abo = await User.get
+  
+    if (!oldUser) {
+      return res.send({ data: "User doesn't exists!!" });
+    }
+    try {
+      await User.updateOne({
+        email: email,
+        //email:email,
+        //rickroll:rickroll
+      }, {
+        name:name,
+      });
+      res.send({ status: "ok", data: "User Updated" });
+    } catch (error) {
+      res.send({ status: "error", data: error });
+    }
+    
+  });
+
+
+
 
   app.post("/sendVideo", async (req, res) => {
     const {email, rickroll} = req.body;
@@ -193,6 +574,62 @@ app.get("/", (req, res) => {
       res.send({ status: "error", data: error });
     }
     
+  });
+
+app.post("/sendWhistle", async (req, res) => {
+    const {email, whistle} = req.body;
+    const oldUser = await User.findOne({ email: email });
+    const abio = await User.find({})
+    //console.log(oldUser)
+    //const abo = await User.get
+  
+    if (!oldUser) {
+      return res.send({ data: "User doesn't exists!!" });
+    }
+    try {
+      await User.updateOne({
+        email: email,
+        //email:email,
+        //rickroll:rickroll
+      }, {
+        whistle:whistle,
+      });
+      res.send({ status: "ok", data: "User Updated", banna:abio });
+    } catch (error) {
+      res.send({ status: "error", data: error });
+    }
+    
+  });
+
+app.post("/sendLebiga", async (req, res) => {
+    const {email, lebiga} = req.body;
+    const oldUser = await User.findOne({ email: email });
+    const abio = await User.find({})
+    //console.log(oldUser)
+    //const abo = await User.get
+  
+    if (!oldUser) {
+      return res.send({ data: "User doesn't exists!!" });
+    }
+    try {
+      await User.updateOne({
+        email: email,
+        //email:email,
+        //rickroll:rickroll
+      }, {
+        lebiga:lebiga,
+      });
+      res.send({ status: "ok", data: "User Updated", banna:abio });
+    } catch (error) {
+      res.send({ status: "error", data: error });
+    }
+    
+  });
+
+
+app.post("/sendData", async (req, res) => {
+    const abio = await User.find({})
+    res.send({ status: "ok", data: "User Updated", banna:abio }); 
   });
 
 app.listen(8080, () => {
